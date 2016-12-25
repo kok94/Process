22ab8c89feccc902b17413993c8ca721542d9dad
kboohan <boohankhoo@gmail.com>
2016-12-26 00:25:45 +0800

    Reservation Report

---
+++ Coding/ReservationShowReport.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReservationShowReport.aspx.cs" Inherits="WEBFINAL.ReservationShowReport" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server"> 
+    <div>
+    
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" Font-Size="8pt" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt">
+            <LocalReport ReportEmbeddedResource="WEBFINAL.ReservationReport.rdlc">
+                <DataSources>
+                    <rsweb:ReportDataSource DataSourceId="ObjectDataSource1" Name="DataSet1" />
+                </DataSources>
+            </LocalReport>
+        </rsweb:ReportViewer>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="GetData" TypeName="ReservationDataSetTableAdapters.ReservationTableAdapter"></asp:ObjectDataSource>
+    
+    </div>
+
+
+        </asp:Content>
---
+++ Coding/ReservationShowReport.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WebForms;
+
+namespace WEBFINAL
+{
+    public partial class ReservationShowReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/ReservationReport.rdlc");
+                ReservationDataSet dsReservation = GetData("select * from Reservation");
+                ReportDataSource datasource = new ReportDataSource("DataSet1", dsReservation.Tables[1]);
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+            }
+        }
+
+        private ReservationDataSet GetData(string query)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString;
+            SqlCommand cmd = new SqlCommand(query);
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+
+                    sda.SelectCommand = cmd;
+                    using (ReservationDataSet dsReservation = new ReservationDataSet())
+                    {
+                        sda.Fill(dsReservation, "DataTable1");
+                        return dsReservation;
+                    }
+                }
+            }
+        }
+    }
+}
